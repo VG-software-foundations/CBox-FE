e4e4739fc47b69b1a5efe5c64e5c6584758e1d92
Darya <d.oduvad@mail.ru>
2024-10-20 15:32:48 +0300

    i18n-2layout

--- src/componentsEnter/enter/Main.jsx
+++ src/componentsEnter/enter/Main.jsx
@@ -1,72 +1,119 @@
-import './Main.css';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import google from './../img/images/flat-color-icons_google@2x.jpg';
 import face from './../img/images/Vector.png';
 import logoMain from './../img/images/Logo.png';
-import languMain from './../img/images/lang.png';
+import './Main.css';
+
+const langArr = {
+    "login-title-enterM activeM": {
+        "ru": "Вход",
+        "en": "Sign in",
+    },
+    "login-title-registrationM passiveM": {
+        "ru": "Регистрация",
+        "en": "Registration",
+    },
+    "lgn-email": {
+        "ru": "Электронная почта:",
+        "en": "Email:",
+    },
+    "pass": {
+        "ru": "Пароль:",
+        "en": "Password:",
+    },
+    "login-btnM": {
+        "ru": "Войти",
+        "en": "Sign in",
+    },
+    "social-loginM": {
+        "ru": "Авторизация через социальные сети",
+        "en": "Authorization via social networks",
+    },
+};
 
 function Main() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
+    
+    const [language, setLanguage] = useState('ru'); 
 
-  const handleLogin = (event) => {
-    event.preventDefault();
-    const email = event.target.email.value;
-    const password = event.target.password.value;
+    const handleLogin = (event) => {
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
 
-    if (email === 'test@example.com' && password === 'password') {
-      navigate('/profile');
-    } else {
-      alert('Неверные учетные данные');
-    }
-  };
+        if (email === 'test@example.com' && password === 'password') {
+            navigate('/profile');
+        } else {
+            alert('Неверные учетные данные');
+        }
+    };
 
-  return (
-    <div className='backM'>
-      <div className="containerMainM">
-        <div className="login-boxM">
-          <div className="choiceM">
-            <h2 className="login-title-enterM activeM">Вход</h2>
-            <h2 className="login-title-registrationM passiveM" onClick={() => navigate('/registration')}>Регистрация</h2>
-          </div>
-          <form onSubmit={handleLogin}>
-            <div className="input-groupM">
-              <label htmlFor="email">Электронная почта:</label>
-              <input type="email" id="email" name="email" required />
+    useEffect(() => {
+        const hash = window.location.hash.substr(1);
+        if (hash && ['en', 'ru'].includes(hash)) {
+            setLanguage(hash);
+        }
+    }, []);
+
+    const changeLanguage = (event) => {
+        const selectedLang = event.target.value;
+        setLanguage(selectedLang);
+        window.location.hash = selectedLang;
+    };
+
+    return (
+        <div className='backM'>
+            <div className="containerMainM">
+                <div className="login-boxM">
+                    <div className="choiceM">
+                        <h2 className="login-title-enterM activeM">{langArr["login-title-enterM activeM"][language]}</h2>
+                        <h2 className="login-title-registrationM passiveM" onClick={() => navigate('/registration')}>
+                            {langArr["login-title-registrationM passiveM"][language]}
+                        </h2>
+                    </div>
+                    <form onSubmit={handleLogin}>
+                        <div className="input-groupM">
+                            <label htmlFor="email" className='lgn-email'>{langArr["lgn-email"][language]}</label>
+                            <input type="email" id="email" name="email" required />
+                        </div>
+                        <div className="input-groupM">
+                            <label htmlFor="password" className='pass'>{langArr["pass"][language]}</label>
+                            <input type="password" id="password" name="password" required />
+                        </div>
+                        <div className="inPutM">
+                            <button type="submit" className="login-btnM">{langArr["login-btnM"][language]}</button>
+                        </div>
+                    </form>
+                    <div className="social-loginM">
+                        <p>{langArr["social-loginM"][language]}</p>
+                        <div className="social-iconsM">
+                            <div className="google">
+                                <img src={google} alt="Google Login" />
+                            </div>
+                            <div className="facebook">
+                                <img src={face} alt="Facebook Login" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="input-groupM">
-              <label htmlFor="password">Пароль:</label>
-              <input type="password" id="password" name="password" required />
+
+            <div className="footer">
+                <div className="container">
+                    <div className="logo">
+                        <img src={logoMain} alt="Logo" />
+                    </div>
+                    <div className="language">
+                        <select className='select' value={language} onChange={changeLanguage}>
+                            <option value="ru">Русский</option>
+                            <option value="en">English</option>
+                        </select>
+                    </div>
+                </div>
             </div>
-            <div className="inPutM">
-              <button type="submit" className="login-btnM">Войти</button>
-            </div>
-          </form>
-          <div className="social-loginM">
-            <p>Авторизация через социальные сети</p>
-            <div className="social-iconsM">
-              <div className="google">
-                <img src={google} alt="Google Login" />
-              </div>
-              <div className="facebook">
-                <img src={face} alt="Facebook Login" />
-              </div>
-            </div>
-          </div>
         </div>
-        
-      </div>
-
-      <div className="footer">
-        <div className="container">
-            <div className="logo">
-            <img src={logoMain}></img>
-            </div><div className="language">
-            <img src={languMain}></img>
-            </div>
-        </div>
-    </div>
-    </div>
-  );
+    );
 }
 
 export default Main;
--- src/componentsRegistration/registration/MainReg.jsx
+++ src/componentsRegistration/registration/MainReg.jsx
@@ -1,74 +1,135 @@
-import './MainReg.css'
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import google from './../img/images/flat-color-icons_google@2x.jpg'
-import face from './../img/images/Vector.png'
-import logoReg from './../img/images/Logo.png'
-import lanReg from './../img/images/lang.png'
+import google from './../img/images/flat-color-icons_google@2x.jpg';
+import face from './../img/images/Vector.png';
+import logoReg from './../img/images/Logo.png';
+import './MainReg.css';
 
-function MainReg(){
+const langArr = {
+    "login-title-enter active": {
+        "ru": "Вход",
+        "en": "Sign in",
+    },
+    "login-title-registration passive": {
+        "ru": "Регистрация",
+        "en": "Registration",
+    },
+    "lgn-email": {
+        "ru": "Электронная почта:",
+        "en": "Email:",
+    },
+    "pass": {
+        "ru": "Пароль:",
+        "en": "Password:",
+    },
+    "confirm-pass": {
+        "ru": "Подтвердите пароль:",
+        "en": "Confirm password:",
+    },
+    "login-btn": {
+        "ru": "Зарегистрироваться",
+        "en": "Register",
+    },
+    "social-login": {
+        "ru": "Авторизация через социальные сети",
+        "en": "Authorization via social networks",
+    },
+};
+
+function MainReg() {
     const navigate = useNavigate();
+    const [language, setLanguage] = useState('ru'); 
 
     const handleLogin = (event) => {
-      event.preventDefault();
-      const email = event.target.email.value;
-      const password = event.target.password.value;
-  
-      if (email === 'test@example.com' && password === 'password') {
-        navigate('/profile');
-      } else {
-        alert('Неверные учетные данные');
-      }
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+
+        if (email === 'test@example.com' && password === 'password') {
+            navigate('/profile');
+        } else {
+            alert('Неверные учетные данные');
+        }
     };
-    
-    return(
+
+    useEffect(() => {
+        const hash = window.location.hash.substr(1);
+        if (hash && ['en', 'ru'].includes(hash)) {
+            setLanguage(hash);
+        }
+    }, []);
+
+    const changeLanguage = (event) => {
+        const selectedLang = event.target.value;
+        setLanguage(selectedLang);
+        window.location.hash = selectedLang;
+    };
+
+    return (
         <div className='back'>
             <div className="containerMain">
                 <div className="login-box">
                     <div className="choice">
-                        <h2 className="login-title-enter active" onClick={() => navigate('/')}>Вход</h2>
-                        <h2 className="login-title-registration passive">Регистрация</h2>
+                        <h2 className="login-title-enter active" onClick={() => navigate('/')}>
+                            {langArr["login-title-enter active"][language]}
+                        </h2>
+                        <h2 className="login-title-registration passive">
+                            {langArr["login-title-registration passive"][language]}
+                        </h2>
                     </div>
                     <form onSubmit={handleLogin}>
                         <div className="input-group">
-                            <label htmlFor="email">Электронная почта:</label>
-                            <input type="email" id="email" name="email" required></input>
+                            <label htmlFor="email" className='lgn-email'>
+                                {langArr["lgn-email"][language]}
+                            </label>
+                            <input type="email" id="email" name="email" required />
                         </div>
                         <div className="input-group">
-                            <label htmlFor="password">Пароль:</label>
-                            <input type="password" id="password" name="password" required></input>
+                            <label htmlFor="password" className='pass'>
+                                {langArr["pass"][language]}
+                            </label>
+                            <input type="password" id="password" name="password" required />
                         </div>
                         <div className="input-group">
-                            <label htmlFor="confirm-password">Подтвердите пароль:</label>
-                            <input type="password" id="confirm-password" name="confirm-password" required></input>
+                            <label htmlFor="confirm-password" className='confirm-pass'>
+                                {langArr["confirm-pass"][language]}
+                            </label>
+                            <input type="password" id="confirm-password" name="confirm-password" required />
                         </div>
                         <div className="inPut">
-                            <button type="submit" className="login-btn">Зарегистрироваться</button>
+                            <button type="submit" className="login-btn">
+                                {langArr["login-btn"][language]}
+                            </button>
                         </div>
                     </form>
                     <div className="social-login">
-                        <p>Авторизация через социальные сети</p>
+                        <p>{langArr["social-login"][language]}</p>
                         <div className="social-icons">
                             <div className="google">
-                                <img src={google} alt="Google Login"></img>
+                                <img src={google} alt="Google Login" />
                             </div>
                             <div className="facebook">
-                                <img src={face} alt="Facebook Login"></img>
+                                <img src={face} alt="Facebook Login" />
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div className='footerReg'>
-               <div className='container'>
-               <div className="logo">
-            <img src={logoReg}></img>
-            </div><div className="language">
-            <img src={lanReg}></img>
-            </div>
-               </div>
+                <div className='container'>
+                    <div className="logo">
+                        <img src={logoReg} alt="Logo" />
+                    </div>
+                    <div className="language">
+                        <select className='select' value={language} onChange={changeLanguage}>
+                            <option value="ru">Русский</option>
+                            <option value="en">English</option>
+                        </select>
+                    </div>
+                </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default MainReg;
